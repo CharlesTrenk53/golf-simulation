38a01eec3297588d53279770fd33569a4deb1a24
CharlesTrenk53 <123709426+CharlesTrenk53@users.noreply.github.com>
2026-08-13 19:42:06 -0400

    test(poc24): prove inter-hole waiting

--- tests/poc24_inter_hole_transition_scheduler_test.gd
+++ tests/poc24_inter_hole_transition_scheduler_test.gd
@@ -1,4 +1,11 @@
 extends SceneTree
+const I=preload("res://simulation/inter_hole_transition_scheduler.gd")
 func _init()->void:
- print("POC-24H TEST SHELL")
+ var s=I.new()
+ var e=s.schedule_transition("follow","lead",1,2,600.0,645.0)
+ assert(bool(e.get("waited_for_group_ahead",false)))
+ assert(is_equal_approx(float(e.get("wait_seconds",0.0)),45.0))
+ assert(s.due_transitions(644.99).is_empty())
+ assert(s.due_transitions(645.01).size()==1)
+ print("POC-24H INTER-HOLE CATCH-UP WAIT SCHEDULING PASSED")
  quit(0)
